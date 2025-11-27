22131a46cbb8c0c98f3db8bef2fe821d01124a61
DESKTOP-GRBB3NJ\Kayque <kayque.original@hotmail.com>
2025-11-27 16:37:53 -0300

    ajustes na main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from utils import *
 
 def process_data(d, t, y, n):
     res = []
@@ -17,5 +18,22 @@ def process_data(d, t, y, n):
     print(res)
     return res
 
+def process_data_two(d, t, y, n):
+    res = []
+    if t == 1:
+        for i in range(len(d)):
+            if d[i] > y:
+                res.append(d[i] * n)
+    elif t == 2:
+        for i in range(len(d)):
+            if d[i] < y:
+                res.append(d[i] + n)
+    else:
+        return None
+    
+    print(res)
+    return res
+
 data = [10, 20, 30, 40, 50, 60]
 process_data(data, 1, 25, 2)
+configure_system_settings("localhost", 8080, "root", "1234", "prod", 10, 3, True, "INFO", "admin@admin.com", "/tmp")
